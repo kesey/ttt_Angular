cdc316cf245086a272b7b78373ceac02933f45a5
FabienMeunier <meunier.fabien@yahoo.fr>
2016-07-26 16:01:57 +0200

    footer

--- proxy.js
+++ proxy.js
@@ -1,12 +1,15 @@
 /**
  * Created by Fabien on 26/07/2016.
  */
+'use strict';
+
 var express = require('express'),
     cors = require('cors'),
+    port = process.env.PORT || 9000,
     app = express();
 
 app
     .use(cors())
-    .listen(9000, function(){
-        console.log('CORS-enabled web server listening on port 9000');
+    .listen(port, function(){
+        console.log('CORS-enabled web server listening on port ' + port);
     });
