9690c886afbe553fc542117d8198a0253ab57e94
FabienMeunier <meunier.fabien@yahoo.fr>
2016-10-21 00:08:34 +0200

    links

--- app/scripts/controllers/links.js
+++ app/scripts/controllers/links.js
@@ -8,8 +8,38 @@
  * Controller of the tttApp
  */
 angular.module('tttApp')
-  .controller('LinksCtrl', ['$rootScope', '$location', function ($rootScope, $location) {
+  .controller('LinksCtrl', ['$rootScope', '$location', 'config', 'server', 'concatArray', function ($rootScope, $location, config, server, concatArray) {
 
     $rootScope.url = $location.path();
+
+    this.loading = true;
+    this.list = [];
+
+    var offset = 0,
+      pagination = config.pagination;
+
+    this.getList = function() {
+      this.busy = true;
+      server
+          .get({object: 'link', action: 'index', param: offset + ', ' + pagination})
+          .$promise
+          .then(function(response) {
+              console.log('server connect success');
+              this.loading = false;
+              concatArray.concat(this.list, response.artistes);
+              var totalArtistes = parseInt(response.totalArtistes[0].total);
+              if (offset < totalArtistes) { // désactive infiniteScroll quand toute la liste est affichée
+                  this.busy = false;
+              }
+              offset += pagination;
+          }.bind(this), function(response) {
+              console.log('server connect fail');
+              this.loading = false;
+              this.failInfos = response.data;
+              this.busy = false;
+          }.bind(this));
+    };
+
+    this.getList();
 
   }]);
